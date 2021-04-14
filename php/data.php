b247ac40d157050eb6dc08cd9b9ef1cbd7f3970c
Virko1312 <stefansekuli@yahoo.com>
2021-04-14 11:21:02 +0200

    Logout part is done, everything is working fine

--- php/data.php
+++ php/data.php
@@ -2,7 +2,7 @@
     while ($row = mysqli_fetch_assoc($sql)){
         $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
                 OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id}
-                OR outgoing_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+                OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
         $query2 = mysqli_query($conn, $sql2);
         $row2 = mysqli_fetch_assoc($query2);
         if (mysqli_num_rows($query2) > 0) {
@@ -13,10 +13,17 @@
 
         //trimming message if words are more than 28 characters
         (strlen($result) > 28) ? $msg = substr($result, 0, 28).'...' : $msg = $result;
+        if(isset($row2['outgoing_msg_id'])){
+            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+        }else{
+            $you = "";
+        }
         //Adding "you:" text before message if login id sent message
-        ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+        ($outgoing_id == $row['unique_id']) ? $hid_me = "hide" : $hid_me = "";
         //check if user is online or offline
         ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
+
+
 
         $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
                                 <div class="content">
